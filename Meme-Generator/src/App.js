875315bb75cf87f52f8024ad2cc6dea2bc44a9b9
JimCoder-Dev <jamesholmson88@gmail.com>
2022-07-02 23:07:58 +0100

    Initial Meme project

---
+++ Meme-Generator/src/App.js
@@ -0,0 +1,12 @@
+import { render } from "react-dom";
+import Header from './Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
